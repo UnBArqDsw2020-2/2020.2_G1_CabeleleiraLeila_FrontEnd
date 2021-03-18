451c400503183a963a4538601250d4ca8ef89555
geraldovictor <geraldovictor@outlook.com>
2021-03-18 18:11:49 -0300

    Renomeia a pasta e arquivos que referenciavam o nome da pasta e adiciona docker ao projeto

--- cabeleleira-leila-web/karma.conf.js
+++ cabeleleira-leila-web/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Cabeleleira-Leila'),
+      dir: require('path').join(__dirname, './coverage/cabeleleira-leila-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
