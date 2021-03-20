ce0e0bfbaa2638c2e1801f39c289e193d5051a97
Rafaella Junqueira <rafaellafjunqueira@gmail.com>
2021-03-20 16:32:43 -0300

    Adiciona componente de login e cadastro
    Co-authored-by: Geraldo Victor <geraldovictor@outlook.com>
    Co-authored-by: Isabella Carneiro <isabellacgmsa1@gmail.com>
    Co-authored-by: Bruna Almeida <brunaalmeida48@gmail.com>

--- cabeleleira-leila-web/src/app/app-routing.module.ts
+++ cabeleleira-leila-web/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
 
 const routes: Routes = [
+  {path: 'login-cadastro', component: LoginCadastroComponent}
 ];
 
 @NgModule({
--- cabeleleira-leila-web/src/app/app.module.ts
+++ cabeleleira-leila-web/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCadastroComponent
   ],
   imports: [
     BrowserModule,
