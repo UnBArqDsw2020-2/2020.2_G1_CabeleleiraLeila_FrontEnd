c0383edb779f601859bdda1f793c2a53029acb0c
Lucas Lopes <l.lopes.fga@gmail.com>
2021-04-04 14:02:19 -0300

    interceptor renomeação
    Co-authored-by: Rafaella Junqueira <rafaellafjunqueira@gmail.com>
    Co-authored-by: Geraldo Victor <geraldovictor@outlook.com>
    Co-authored-by: Isabella Carneiro <isabellacgmsa1@gmail.com>
    Co-authored-by: Kalebe Lopes <calbkalebe@gmail.com>
    Co-authored-by: Damarcones Porto <damarcones@gmail.com>
    Co-authored-by: Erick Giffoni <giffoni.erick@gmail.com>
    Co-authored-by: Bruna Almeida <brunaalmeida48@gmail.com>

--- cabeleleira-leila-web/src/app/_helpers/auth.interceptor.ts
+++ cabeleleira-leila-web/src/app/_helpers/auth.interceptor.ts
@@ -2,18 +2,18 @@ import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
-import { TokenStorageService } from '../_services/token-storage.service';
 import { Observable } from 'rxjs';
+import { TokenStorageService } from '../_services-auth/token-storage.service';
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: TokenStorageService) { }
+  constructor(private tokenStorageService: TokenStorageService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = this.token.getToken();
+    const token = this.tokenStorageService.getToken();
     if (token != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
     }
